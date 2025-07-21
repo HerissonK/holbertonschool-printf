fb3d1096e2be3d3fedc7745c137d63bfc3e5a259
HerissonK <11513@Holbertonstudents.com>
2025-07-21 12:20:34 +0000

    premier test de la fonmction _printf pour %c %s et %%

--- _printf.c
+++ _printf.c
@@ -2,26 +2,52 @@
 #include "main.h"
 #include <stdarg.h>
 
+/**
+ * pc - print a char
+ * @args: the argument to print
+ */
+
 void pc(va_list args)
 {
-	char c = va_arg(args,int);
+	char c = va_arg(args, int);
+
 	_putchar(c);
 }
+
+/**
+ * ps - print a string
+ * @args: the argument to print
+ */
+
 void ps(va_list args)
 {
 	char *s = va_arg(args, char*);
+
 	while (*s)
 		_putchar(*s++);
 }
+
+/**
+ * pp - print nothing
+ * @args: print nothing
+ */
+
 void pp(va_list args)
 {
 	(void)args;
 	_putchar('%');
 }
+
+/**
+ * _printf - function that produces output according to a format
+ * @format: format to get the right printf
+ * Return: always 0
+ */
+
 int _printf(const char *format, ...)
 {
 	va_list parameter;
-	int count = 0;
+
 	int i = 0;
 
 	funckey checker[3] = {
@@ -29,7 +55,7 @@ int _printf(const char *format, ...)
 		{ps, 's'},
 		{pp, '%'}
 	};
-	va_start (parameter, format);
+	va_start(parameter, format);
 	while (*format)
 	{
 		if (*format == '%' && *(format + 1))
@@ -39,17 +65,23 @@ int _printf(const char *format, ...)
 			for (i = 0; i < 3; i++)
 			{
 				if (checker[i].spec == *format)
+				{
 					checker[i].f(parameter);
-				break;
+					break;
+				}
 			}
 			if (i == 3)
+			{
 				_putchar('%');
-			_putchar(*format);
+				_putchar(*format);
+			}
 		}
 		else
+		{
 			_putchar(*format);
+		}
 		format++;
 	}
 	va_end(parameter);
-	return count;
+	return (0);
 }
--- main.h
+++ main.h
@@ -4,6 +4,14 @@
 int _putchar(char c);
 int _printf(const char *format, ...);
 
+/**
+ * struct funckey - struct to find the argument
+ *
+ * @f: parameter to use
+ * @spec: comparation with the string
+ */
+
+
 typedef struct funckey
 {
 	void (*f)(va_list);
