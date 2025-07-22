74c8e2d453f75c6fc11fb897ae211650989550f6
HerissonK <11513@Holbertonstudents.com>
2025-07-22 08:05:20 +0000

    deplacement des fonctions pc, ps et pp dans un fichier main.function.c

--- _printf.c
+++ _printf.c
@@ -2,51 +2,7 @@
 #include "main.h"
 #include <stdarg.h>
 
-/**
- * pc - print a char
- * @args: the argument to print
- * Return: 1
- */
-
-int pc(va_list args)
-{
-	_putchar(va_arg(args, int));
-	return (1);
-}
-
-/**
- * ps - print a string
- * @args: the argument to print
- * Return: 1
- */
-
-int ps(va_list args)
-{
-	int index;
-	char *s = va_arg(args, char *);
-
-	if (s == NULL)
-		s = "(null)";
-	for (index = 0; s[index] != '\0'; index++)
-		_putchar(s[index]);
-	return (index);
-}
-
-/**
- * pp - print nothing
- * @args: print nothing
- * Return: 1
- */
-
-int pp(va_list args)
-{
-	(void)args;
-	_putchar('%');
-	return (1);
-}
-
-/**
- * _printf - function that produces output according to a format
+/** _printf - function that produces output according to a format
  * @format: format to get the right printf
  * Return: always 0
  */
