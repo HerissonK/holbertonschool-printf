feb73fc4510f5240f5eccf98754df2f878b6fa55
‘Boulnois’ <georgia.boulnois@gmail.com>
2025-07-23 08:47:42 +0000

    Creation of the man page for the function _printf

--- get_number.c
+++ get_number.c
@@ -37,31 +37,3 @@ int signed_num(va_list args)
 
 	return (len);
 }
-
-/**
- *
- */
-
-
-int print_unsigned_number(unsigned int list)
-{
-	int len, div;
-	unsigned int num;
-
-	div = 1;
-	len = 0;
-
-	num = list;
-
-	for (; num / div > 9; )
-		div *= 10;
-
-	for (; div != 0; )
-	{
-		len += _putchar('0' + num / div);
-		num %= div;
-		div /= 10;
-	}
-
-	return (len);
-}
